85bb99400e73d8a0b8a97360dbc2cac1a03b5654
Kosolapov Vadim <vadkos33@outlook.com>
2019-05-13 14:48:13 +0200

    Shortened code in tests

--- name_test.go
+++ name_test.go
@@ -1,8 +1,8 @@
 package faker
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestRandSex(t *testing.T) {
@@ -16,58 +16,48 @@ func TestRandSex(t *testing.T) {
 }
 
 func TestName(t *testing.T) {
-	language := []string{"en", "ru", "de"}
-	for _, v := range language {
-		name := Name(v)
-		ty := reflect.TypeOf(name).Kind()
-		if ty != reflect.String {
-			t.Error("Error of return name")
-		}
+	language := RandomLanguage()
+	name := Name(language)
+	ty := reflect.TypeOf(name).Kind()
+	if ty != reflect.String {
+		t.Error("Error of return name")
 	}
 }
 
 func TestFirstName(t *testing.T) {
-	language := []string{"en", "ru", "de"}
-	for _, v := range language {
-		name := FirstName(v)
-		ty := reflect.TypeOf(name).Kind()
-		if ty != reflect.String {
-			t.Error("Error of return name")
-		}
+	language := RandomLanguage()
+	name := FirstName(language)
+	ty := reflect.TypeOf(name).Kind()
+	if ty != reflect.String {
+		t.Error("Error of return name")
 	}
 }
 
 func TestLastName(t *testing.T) {
-	language := []string{"en", "ru", "de"}
-	for _, v := range language {
-		name := LastName(v)
-		ty := reflect.TypeOf(name).Kind()
-		if ty != reflect.String {
-			t.Error("Error of return name")
-		}
+	language := RandomLanguage()
+	name := LastName(language)
+	ty := reflect.TypeOf(name).Kind()
+	if ty != reflect.String {
+		t.Error("Error of return name")
 	}
 }
 
 func TestSexFirstName(t *testing.T) {
-	language := []string{"en", "ru", "de"}
 	sex := RandSex()
-	for _, v := range language {
-		name := SexFirstName(v, sex)
-		ty := reflect.TypeOf(name).Kind()
-		if ty != reflect.String {
-			t.Error("Error of return name")
-		}
+	language := RandomLanguage()
+	name := SexFirstName(language, sex)
+	ty := reflect.TypeOf(name).Kind()
+	if ty != reflect.String {
+		t.Error("Error of return name")
 	}
 }
 
 func TestSexLastName(t *testing.T) {
-	language := []string{"en", "ru", "de"}
+	language := RandomLanguage()
 	sex := RandSex()
-	for _, v := range language {
-		name := SexLastName(v, sex)
-		ty := reflect.TypeOf(name).Kind()
-		if ty != reflect.String {
-			t.Error("Error of return name")
-		}
+	name := SexLastName(language, sex)
+	ty := reflect.TypeOf(name).Kind()
+	if ty != reflect.String {
+		t.Error("Error of return name")
 	}
 }
